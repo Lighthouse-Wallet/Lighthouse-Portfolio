28b6d06f6a6e3b99b9cc7763d035e72203799be0
Tigran Asriyan <tigran.asriyan@gmail.com>
2021-12-14 11:04:03 +0400

    change swagger url to root

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ application = app = Flask(__name__)
 CORS(app)
 
 ### swagger specific ###
-SWAGGER_URL = '/swagger'
+SWAGGER_URL = '/'
 API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
